72db72d2b63d72c95ebf28c38b03e18335e8b00b
srinivasareddy0987654321 <srinivasareddy0987654321@gmail.com>
2025-02-22 21:05:06 +0530

    created pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 
-
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
 export default function App() {
   return (
     <div>
-      
+      <BrowserRouter>
+      <Routes>
+        <Route/>
+      </Routes>
+      </BrowserRouter>
     </div>
   )
 }
